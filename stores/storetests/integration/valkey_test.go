1d1da6f231ea714c7c9aacb2b7fd654aba0b1bdb
yikakia <yika_w@outlook.com>
2026-03-01 20:10:01 +0800

    test: integration test for valkey

---
+++ stores/storetests/integration/valkey_test.go
@@ -0,0 +1,125 @@
+package integration
+
+import (
+	"bytes"
+	"context"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	tclog "github.com/testcontainers/testcontainers-go/log"
+	tcvalkey "github.com/testcontainers/testcontainers-go/modules/valkey"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"github.com/valkey-io/valkey-go"
+	"github.com/yikakia/cachalot/core/cache"
+	"github.com/yikakia/cachalot/stores/storetests"
+	store_valkey "github.com/yikakia/cachalot/stores/valkey"
+)
+
+func newValkeyClient(t *testing.T) valkey.Client {
+	ctx := context.Background()
+	exposedPort := "6379/tcp"
+
+	waitReadyCmd := []string{
+		"valkey-cli",
+		"ping",
+	}
+
+	opts := []testcontainers.ContainerCustomizer{
+		testcontainers.WithLogger(tclog.TestLogger(t)),
+		testcontainers.WithWaitStrategy(
+			wait.ForAll(
+				wait.ForLog("* Ready to accept connections"),
+				wait.ForExposedPort(),
+				wait.ForListeningPort(nat.Port(exposedPort)),
+				wait.ForExec(waitReadyCmd),
+			),
+		),
+		// attempt to reuse this container
+		testcontainers.CustomizeRequest(testcontainers.GenericContainerRequest{
+			ContainerRequest: testcontainers.ContainerRequest{
+				Name:     "valkey",
+				Hostname: "localhost",
+			},
+			Started: true,
+			Reuse:   true,
+		}),
+	}
+
+	valkeyContainer, err := tcvalkey.Run(ctx, "valkey/valkey:latest", opts...)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, valkeyContainer.Terminate(context.TODO()))
+	})
+
+	endpoint, err := valkeyContainer.ConnectionString(context.TODO())
+	require.NoError(t, err)
+
+	valkeyURL, err := url.Parse(endpoint)
+	require.NoError(t, err)
+
+	client, err := valkey.NewClient(valkey.ClientOption{InitAddress: []string{valkeyURL.Host}})
+	require.NoError(t, err)
+
+	return client
+}
+
+func newValkeyStore(t *testing.T) cache.Store {
+	return store_valkey.New(newValkeyClient(t),
+		store_valkey.WithClientSideCacheExpiration(time.Second))
+}
+
+func TestValkey(t *testing.T) {
+	storetests.RunStoreTestSuites(t, newValkeyStore,
+		storetests.WithWaitingAfterWrite(func(t *testing.T, _ cache.Store) {
+			if t.Name() == "NonEmptyStore" {
+				// Todo 对于 NonEmptyStore 这个测试，flush 可能会出现第一个key不能flush的场景
+				// 采用暂停的方式保证能通过调用 单独把这部分逻辑提出来测试却不能复现 不确定是什么问题
+				time.Sleep(time.Millisecond * 3500)
+			}
+		}),
+		storetests.WithEncodeSetValue(func(v string) any {
+			return []byte(v)
+		}),
+		storetests.WithAssertValue(func(t *testing.T, got any, expected string) {
+			raw, ok := got.([]byte)
+			if !ok {
+				t.Fatalf("want []byte got %T", got)
+			}
+			assert.True(t, bytes.Equal(raw, []byte(expected)))
+		}))
+}
+
+//func TestFlush(t *testing.T) {
+//	for range 100 {
+//		t.Run(strconv.Itoa(100), func(t *testing.T) {
+//			vc := newValkeyClient(t)
+//
+//			keys := []string{"key1", "key2", "key3"}
+//			for _, key := range keys {
+//				cmd := vc.B().Set().Key(key).Value(key + "value").Build()
+//				err := vc.Do(context.Background(), cmd).Error()
+//				assert.NoError(t, err)
+//			}
+//
+//			for _, key := range keys {
+//				cmd := vc.B().Get().Key(key).Build()
+//				err := vc.Do(context.Background(), cmd).Error()
+//				assert.NoError(t, err)
+//			}
+//
+//			cmd := vc.B().Flushall().Sync().Build()
+//			require.NoError(t, vc.Do(context.Background(), cmd).Error())
+//
+//			for _, key := range keys {
+//				cmd := vc.B().Get().Key(key).Build()
+//				err := vc.Do(context.Background(), cmd).Error()
+//				assert.ErrorIs(t, err, valkey.Nil, "key %s should be deleted", key)
+//			}
+//		})
+//	}
+//}
